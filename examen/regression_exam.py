83def3e267f2c4a6f3c2c34b34143eee08d91f09
jimlopezy <jimmy_ran.lopez@uao.edu.co>
2026-09-12 12:38:47 -0500

    respuesta en regression_exam.py

--- examen/regression_exam.py
+++ examen/regression_exam.py
@@ -83,31 +83,30 @@ print(error_norm)
 # ------------------------------------------------------------
 
 # 1. ¿Qué representa cada fila de X?
-# Respuesta:
+# Respuesta: Es una represatación de cada observación en el conjunto de datos, donde la primera columna es un término constante (1) y la segunda columna es el valor de la variable independiente x.
+# Ejemplo [1, 8] 
 
 
 # 2. ¿Por qué X contiene una primera columna de unos?
-# Respuesta:
+# Respuesta: Porque la columna de X tiene uno es para el intercepto y que no cambie el valor cuando se realice la multiplicacion de matrices
 
 
 # 3. ¿Cuál es la dimensión de X.T @ X y por qué?
-# Respuesta:
+# Respuesta: La dimensión de X.T @ X es (2, 2) porque X tiene 8 filas y 2 columnas, entonces al multiplicar la transpuesta de X (2, 8) por X (8, 2) obtenemos una matriz de 2x2.
 
 
 # 4. ¿Qué representa beta_0 dentro de este problema?
-# Respuesta:
+# Respuesta: beta_0 representa el valor esperado de y cuando es x=0
 
 
 # 5. ¿Qué representa beta_1 dentro de este problema?
-# Respuesta:
+# Respuesta: beta_1 se representa cuánto cambia y cuando 0 aumenta una unidad.
 
 
 # 6. ¿Qué significa que la norma del error sea pequeña?
-# Respuesta:
+# Respuesta: Se indica que en el conjunto, las predicciones están relativamente cerca de los valores reales.
 
 
 # 7. ¿Por qué X @ beta permite obtener todas las
 #    predicciones simultáneamente?
-# Respuesta: 
-
-
+# Respuesta: Porque X @ beta es una operación matricial que aplica la transformación lineal definida por beta a toda la matriz X, resultando en un vector con todas las predicciones de todas las observaciones.
