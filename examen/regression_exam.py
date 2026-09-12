fd13f73d4ca54b308405490ab62c402443dae110
Sebastian Moreno <sebastian.moreno@uao.edu.co>
2026-09-12 10:50:42 -0500

    Primer commit del examen, cálculos realizados

--- examen/regression_exam.py
+++ examen/regression_exam.py
@@ -4,47 +4,49 @@ import numpy as np
 # Álgebra lineal aplicada a regresión lineal
 # ============================================================
 
-# Nombre:
-# Apellido 1:
-# Apellido 2:
-# Rama:
+# Nombre: Sebastián 
+# Apellido 1: Moreno
+# Apellido 2: Dorado
+# Rama: moreno_dorado
 
 
 # 1. CARGA DE DATOS
 # ------------------------------------------------------------
 
-data =
+#data = np.loadtxt("data.txt", delimiter=",") 
 
-x =
-y =
+x = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+y = np.array([3.2, 4.8, 7.3, 8.7, 11.1, 12.8, 15.2, 16.7])  
 
 
 # 2. MATRIZ DE DISEÑO
 # ------------------------------------------------------------
 
-X =
+X = np.column_stack((np.ones(len(x)), x))
 
 print("X:")
 print(X)
+print("Shape X:", X.shape)
 
-print("Shape X:", )
-print("Shape y:", )
+print("y:")
+print(y)
+print("Shape y:", y.shape)
 
 
 # 3. OPERACIONES MATRICIALES
 # ------------------------------------------------------------
 
-XtX =
-Xty =
+XtX = X.T @ X
+Xty = X.T @ y
 
 
 # 4. ESTIMACIÓN DE PARÁMETROS
 # ------------------------------------------------------------
 
-beta =
+beta = np.linalg.inv(X.T @ X) @ X.T @ y
 
-beta_0 =
-beta_1 =
+beta_0 = beta[0]
+beta_1 = beta[1]
 
 print("Beta 0:", beta_0)
 print("Beta 1:", beta_1)
@@ -55,7 +57,7 @@ print("Beta 1:", beta_1)
 
 x_new = np.array([1, 9])
 
-prediction =
+prediction = x_new @ beta
 
 print("Prediction:", prediction)
 
@@ -63,14 +65,13 @@ print("Prediction:", prediction)
 # 6. PREDICCIONES DEL DATASET
 # ------------------------------------------------------------
 
-y_pred =
-
+y_pred = X @ beta
 
 # 7. ERROR
 # ------------------------------------------------------------
 
-errors =
-error_norm =
+errors = y - y_pred
+error_norm = np.linalg.norm(errors)
 
 print("Error vector:")
 print(errors)
