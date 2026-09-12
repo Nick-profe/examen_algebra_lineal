6c50bb6b41ed7bcc62d92bd4101bf05897a8fb36
Juan Galindez <juan.galindez@uao.edu.co>
2026-09-12 11:17:37 -0500

    Parcial Finalizado

--- examen/regression_exam.py
+++ examen/regression_exam.py
@@ -87,27 +87,36 @@ print(error_norm)
 # 1. ¿Qué representa cada fila de X?
 # Respuesta:
 
+# la primera fila representa una fila de unos que para el modelo de ecuacion lineal 
+# son muy ultiles para dejar intacta la ecuacion lineal y la segunda fila representa los datos de publicidad que se tienen en el dataset
 
 # 2. ¿Por qué X contiene una primera columna de unos?
 # Respuesta:
+# La primera columna de unos en X representa el término independiente (intercepto) del modelo de regresión lineal.
 
 
 # 3. ¿Cuál es la dimensión de X.T @ X y por qué?
 # Respuesta:
+# la dimensión de X.T @ X es (2, 2) porque X tiene 2 columnas, y al multiplicar la transpuesta de X (que tiene
+# 2 filas) por X (que tiene 2 columnas), se obtiene una matriz de dimensión (2, 2).
 
 
 # 4. ¿Qué representa beta_0 dentro de este problema?
 # Respuesta:
-
+# beta_0 representa el intercepto de la línea de regresión 
 
 # 5. ¿Qué representa beta_1 dentro de este problema?
 # Respuesta:
+# beta_1 representa la pendiente de la línea de regresión, es decir, el cambio esperado en las ventas por cada unidad adicional de publicidad.
 
 
 # 6. ¿Qué significa que la norma del error sea pequeña?
 # Respuesta:
+# significa que las predicciones del modelo sean buenas, muy cercanas a la medido
 
 
 # 7. ¿Por qué X @ beta permite obtener todas las
 #    predicciones simultáneamente?
 # Respuesta:
+# porque al multiplicar la matriz de diseño X por el vector de parámetros beta, se obtiene un vector de predicciones
+# que contiene las predicciones para todos los datos de entrada en el dataset
