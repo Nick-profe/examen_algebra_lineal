225c30cc31404d4c01133ec1789309679d1fd695
velascopripra <juan_d.velasco@uao.edu.co>
2026-09-12 10:50:39 -0500

    Se cargaron los datos a partir del CSV, luego se declararon los vectores a partir de esos datos, se creó la matriz X, se hicieron las operaciones matriciales, se calculó el beta, y se hicieron las predicciones, la predicción inicial y la predicción completa del dataset, y finalmente se calcularon los errores (erros - error_norm)

--- examen/regression_exam.py
+++ examen/regression_exam.py
@@ -4,47 +4,50 @@ import numpy as np
 # Álgebra lineal aplicada a regresión lineal
 # ============================================================
 
-# Nombre:
-# Apellido 1:
-# Apellido 2:
-# Rama:
+# Nombre: Juan David
+# Apellido 1: Velasco
+# Apellido 2: Otero
+# Rama: velasco_otero
 
+import numpy as np
 
 # 1. CARGA DE DATOS
 # ------------------------------------------------------------
 
-data =
+data = np.genfromtxt('../data/sales_data.csv', delimiter=',', skip_header=1)
 
-x =
-y =
-
+x = data [:,[0]]
+y = data [:,[1]]
 
 # 2. MATRIZ DE DISEÑO
 # ------------------------------------------------------------
 
-X =
+X = np.column_stack((np.ones(len(x)), x))
 
 print("X:")
 print(X)
 
-print("Shape X:", )
-print("Shape y:", )
+print("Shape X:", X)
+print("Shape y:", y)
 
 
 # 3. OPERACIONES MATRICIALES
 # ------------------------------------------------------------
 
-XtX =
-Xty =
+Xt=X.T
+
+XtX = Xt @ X
+Xty = Xt @ y
 
 
 # 4. ESTIMACIÓN DE PARÁMETROS
 # ------------------------------------------------------------
 
-beta =
+beta = np.linalg.inv(XtX) @ Xty
+print(beta)
 
-beta_0 =
-beta_1 =
+beta_0 = beta[0]
+beta_1 = beta[1]
 
 print("Beta 0:", beta_0)
 print("Beta 1:", beta_1)
@@ -55,7 +58,7 @@ print("Beta 1:", beta_1)
 
 x_new = np.array([1, 9])
 
-prediction =
+prediction = x_new @ beta
 
 print("Prediction:", prediction)
 
@@ -63,14 +66,16 @@ print("Prediction:", prediction)
 # 6. PREDICCIONES DEL DATASET
 # ------------------------------------------------------------
 
-y_pred =
+y_pred = X @ beta
 
+print("Predictions:")
+print(y_pred)
 
 # 7. ERROR
 # ------------------------------------------------------------
 
-errors =
-error_norm =
+errors = y - y_pred
+error_norm = np.linalg.norm(errors)
 
 print("Error vector:")
 print(errors)
