7fea843ee0477884012a74c2f82b9269f2777e6d
Eduardo Sarmiento <eduardo.sarmiento_p@uao.edu.co>
2026-09-13 11:51:01 -0500

    Adjunto la carga de datos del examen de algebra lineal aplicada a regresion lineal

--- examen/regression_exam.py
+++ examen/regression_exam.py
@@ -4,47 +4,48 @@ import numpy as np
 # Álgebra lineal aplicada a regresión lineal
 # ============================================================
 
-# Nombre:
-# Apellido 1:
-# Apellido 2:
-# Rama:
+# Nombre:Eduardo
+# Apellido 1:Sarmiento
+# Apellido 2:Pradilla
+# Rama: sarmiento_pradilla
 
 
 # 1. CARGA DE DATOS
 # ------------------------------------------------------------
 
-data =
+data = np.array([[1, 3.2], [2, 4.8], [3,7.3], [4, 8.7], [5, 11.1], [6, 12.8], [7, 15.2], [8, 16.7]])
 
-x =
-y =
+
+x = np.array([10, 20, 30, 40, 50])
+y = np.array([3.2, 4.8, 7.3, 8.7, 11.1, 12.8, 15.2, 16.7])
 
 
 # 2. MATRIZ DE DISEÑO
 # ------------------------------------------------------------
 
-X =
+X = np.column_stack((np.ones(len(x)), x))
 
 print("X:")
 print(X)
 
-print("Shape X:", )
-print("Shape y:", )
+print("Shape X:", X.shape)
+print("Shape y:", y.shape)
 
 
 # 3. OPERACIONES MATRICIALES
 # ------------------------------------------------------------
 
-XtX =
-Xty =
+XtX = X.T @ X
+Xty = X.T @ y
 
 
 # 4. ESTIMACIÓN DE PARÁMETROS
 # ------------------------------------------------------------
 
-beta =
+beta = np.linalg.inv(X.T @ X) @ X.T @ y
 
-beta_0 =
-beta_1 =
+beta_0 = beta[0]
+beta_1 = beta[1]
 
 print("Beta 0:", beta_0)
 print("Beta 1:", beta_1)
@@ -55,7 +56,7 @@ print("Beta 1:", beta_1)
 
 x_new = np.array([1, 9])
 
-prediction =
+prediction = x_new @ beta
 
 print("Prediction:", prediction)
 
@@ -63,14 +64,16 @@ print("Prediction:", prediction)
 # 6. PREDICCIONES DEL DATASET
 # ------------------------------------------------------------
 
-y_pred =
+y_pred =X @ beta
 
+print("Predicciones para el dataset:")
+print(y_pred)
 
 # 7. ERROR
 # ------------------------------------------------------------
 
-errors =
-error_norm =
+errors = y - y_pred
+error_norm = np.linalg.norm(errors)
 
 print("Error vector:")
 print(errors)
