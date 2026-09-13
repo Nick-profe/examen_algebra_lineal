100a3f8ee8ba0fa85af31e9664eb869235c4c930
Edwin Guerrero Diaz <edwingd18@gmail.com>
2026-09-12 20:23:10 -0500

    Se responden todas las preguntas

--- examen/regression_exam.py
+++ examen/regression_exam.py
@@ -38,7 +38,6 @@ print("Shape y:", y.shape)
 XtX = X.T @ X
 Xty = X.T @ y
 
-
 # 4. ESTIMACIÓN DE PARÁMETROS
 # ------------------------------------------------------------
 
@@ -75,6 +74,7 @@ y_pred = X @ beta
 errors = y - y_pred
 error_norm = np.linalg.norm(errors)
 
+
 print("Error vector:")
 print(errors)
 
@@ -86,29 +86,43 @@ print(error_norm)
 # ------------------------------------------------------------
 
 # 1. ¿Qué representa cada fila de X?
-# Respuesta:
-
+# Respuesta: Cada fila de X representa una observación. En ella hay 2 valores:
+# el primero siempre será un 1, el cual hace referencia al intercepto, y el otro
+# es una cantidad de publicidad.
 
 # 2. ¿Por qué X contiene una primera columna de unos?
-# Respuesta:
-
+# Respuesta: Porque en la fórmula ventas = beta[0] + beta[1] * publicidad,
+# el uno multiplica a beta[0], y multiplicar por 1 no cambia su valor.
+# Si el uno desaparece, beta[0] no aportaría nada a la fórmula: con
+# publicidad = 0, el modelo predeciría 0 ventas, y la recta tendría
+# que pasar por el punto (0, 0). Por ende, el uno es necesario para
+# incluir beta[0] (el intercepto) en el modelo.
 
 # 3. ¿Cuál es la dimensión de X.T @ X y por qué?
-# Respuesta:
+# Respuesta:La dimension es (2,2) debido a las dimensiones de afuera
+#determinan el resultado y la multiplicacion es (2,8)(8,2)
 
 
 # 4. ¿Qué representa beta_0 dentro de este problema?
-# Respuesta:
+# Respuesta: Teoricamente beta[0] significa el valor esperado de y(ventas) cuando
+# x(publicidad) es 0, en una oracion es reprensenta la cantidad de ventas cuando la
+# la publicidad es 0
 
 
 # 5. ¿Qué representa beta_1 dentro de este problema?
-# Respuesta:
+# Respuesta: Este representa cuanto cambia y, cuando aumenta x, en este caso ccuanto
+# cambia las ventas cuando aumenta la publicidad
 
 
 # 6. ¿Qué significa que la norma del error sea pequeña?
-# Respuesta:
+# Respuesta: Cada error es la diferencia entre el valor real y el valor de la predicion,
+# que la norma sea pequeña significa que las predicciones están conn un valor muy cercana
+# de los valores reales, es decir, que el modelo preedice bien los datos.
 
 
 # 7. ¿Por qué X @ beta permite obtener todas las
 #    predicciones simultáneamente?
-# Respuesta:
+# Respuesta: Porque cada fila de X es una observación, y al multiplicar X @ beta
+# cada fila se multiplica con beta: 1 · beta_0 + publicidad · beta_1, que es
+# la prediccion de esa observación. Como X tiene 8 filas, se obtienen 8
+# resultados en una sola operación. A esto se le llama vetorizaxion.
